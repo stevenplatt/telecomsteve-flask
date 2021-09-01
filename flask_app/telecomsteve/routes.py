0e60fae938ec39039a697ca338b4f1a06b16d68d
Steven Platt <mr.platt@gmail.com>
2021-08-31 23:54:08 -0700

    updated for functional news page

--- flask_app/telecomsteve/routes.py
+++ flask_app/telecomsteve/routes.py
@@ -1,69 +1,36 @@
 from flask import render_template, url_for, flash, redirect, request, session, abort 
 from telecomsteve import app, db
 # from telecomsteve.db_models import Contract, Peer, Host, Block
-from random import randint
-from datetime import datetime
+from telecomsteve.hackernews import Item, User, HackerNews
+# from random import randint
+# from datetime import datetime
 import socket
+import threading
+import math
 
 def get_host_ip():
     host_name = socket.gethostname() 
     host_ip = socket.gethostbyname(host_name) 
     return host_ip
 
-# def get_peer_id():
-#     my_peer_id = randint(100000000000, 999999999999)
-#     return my_peer_id
-
-# about = [
-#     {
-#         'version': '0.2',
-#         'author': 'Steven Platt, Universitat Pompeu Fabra',
-#         'location': 'Barcelona, Spain'
-#     }
-# ]
+def news_singleton(num):
+    hn = HackerNews()
+    stories = hn.top_stories()
+    news_dict = {}
+    try:
+        website = hn.item(stories[num])
+        news_dict[str(website.title)] = str(website.url)
+    except:
+        pass
+    return(news_dict) 
 
 @app.route("/")
-@app.route("/blockchain", methods=["GET", "POST"])
 def home():
     # if not session.get('logged_in'):
     #     return render_template('login_form.html', title='Login')
 
     if not session.get('logged_in'):
         return render_template('index.html')
-
-    elif request.method == "POST" and "ssid" in request.form:
-
-        contract = request.form
-
-        new_contract = Contract(contract_id=randint(1000000000, 9999999999), ssid=contract.get("ssid"), spectrum_block=contract.get("spectrum_block"))
-        db.session.add(new_contract)
-        db.session.commit()
-        print("this method is for new contract")
-
-        return redirect(url_for('home'))
-
-    elif request.method == "POST" and "permission" in request.form:
-
-        block = request.form
-
-        new_block = Block(time_stamp=datetime.utcnow(), creator=block.get("my_hostid"), host_id=block.get("host_id"), contract=block.get("contract_id"), block_action=block.get("permission"))
-        db.session.add(new_block)
-        db.session.commit()
-        print("this method is for new block")
-
-        return redirect(url_for('home'))
-
-    elif request.method == "POST" and "contract_id" in request.form:
-
-        contract = request.form
-
-        print("this method is for existing contract")
-        return redirect(url_for('home'))
-
-    elif request.method == "POST" and "tps" in request.form:
-
-        print("this method is for benchmarking")
-        return redirect(url_for('home'))
 
     else:
 
@@ -80,27 +47,56 @@ def home():
         return render_template('index.html')
 
 
-@app.route("/news")
+@app.route("/news", methods=["GET"])
 def news():
-        return render_template('news.html')
+    count = range(20) # number of stories to display
+    nthreads = 40 # number 
+
+    def worker(count, outdict):
+        """ The worker function, invoked in a thread. 'nums' is a
+            list of numbers to factor. The results are placed in
+            outdict.
+        """
+        for num in count:
+            outdict[num] = news_singleton(num)
+
+    # Each thread will get 'chunksize' nums and its own output dict
+    chunksize = int(math.ceil(len(count) / float(nthreads)))
+    threads = []
+    outs = [{} for i in range(nthreads)]
+
+    for i in range(nthreads):
+        # Create each thread, passing it its chunk of numbers to factor
+        # and output dict.
+        t = threading.Thread(
+                target=worker,
+                args=(count[chunksize * i:chunksize * (i + 1)],
+                      outs[i]))
+        threads.append(t)
+        t.start()
+
+    # Wait for all threads to finish
+    for t in threads:
+        t.join()
+
+    # Merge all partial output dicts into a single dict and return it
+    output = ({k: v for out_d in outs for k, v in out_d.items()})
+
+    return render_template('news.html', news=output)
 
 @app.route("/portfolio")
 def portfolio():
-        return render_template('portfolio.html')
+    return render_template('portfolio.html')
 
 @app.route("/research")
 def research():
-        return render_template('research.html')
+    return render_template('research.html')
 
 @app.route("/resume")
 def resume():
-        return render_template('resume.html')
+    return render_template('resume.html')
 
 
-# @app.route("/map")
-# def map():
-
-#         return render_template('map.html', title='Map')
 
 # @app.route("/nodes", methods=["GET", "POST"])
 # def nodes():
