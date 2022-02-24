a7fb99c950938589b326d49e56a0b3fde19d6922
Steven Platt <31355889+stevenplatt@users.noreply.github.com>
2022-02-24 15:03:08 -0800

    Revert "initial project push"
    This reverts commit 514c857b6baa6ca6dcc7c95a354e74cb58f6aad5.

--- telecomsteve/application.py
+++ telecomsteve/application.py
@@ -3,15 +3,91 @@
 # push updated deployment using 'eb deploy' from within the folder telecomsteve/
 
 from flask import Flask, render_template
+from hackernews import HackerNews
+import threading
+import math
+from urllib.parse import urlparse
 
 application = Flask(__name__)
+
+# list of domains and titles to filter from the news feed
+blocked_terms = ['twitter.com', 'bloomberg.com', 'nytimes.com', 'wsj.com', 'ft.com', 'trump', 'hiring', 'economist.com']
+
+def news_singleton(num):
+    hn = HackerNews()
+    stories = hn.top_stories()
+    news_dict = {"Title":[],"URL":[],"Domain":[], "Score":[]}
+
+    try:
+        website = hn.item(stories[num])
+
+        # instructions: https://stackoverflow.com/questions/1521592/get-root-domain-of-link
+        parsed_uri = urlparse(str(website.url))
+        domain = '{uri.netloc}'.format(uri=parsed_uri)
+        domain = domain.replace('www.', '')
+
+        # add story details to dictionary
+        news_dict["Title"] = (str(website.title))
+        news_dict["URL"] = (str(website.url))
+        news_dict["Domain"] = domain
+        news_dict["Score"] = (str(website.score))
+    except:
+        pass
+    
+    return(news_dict) 
 
 @application.route("/")
 def home():
     return render_template('index.html')
 
+@application.route("/news", methods=["GET"])
+def news():
+    count = range(50) # number of stories to display
+    nthreads = 50 # number 
 
+    def worker(count, outdict):
+        """ The worker function, invoked in a thread. 'nums' is a
+            list of numbers to factor. The results are placed in
+            outdict.
+        """
+        for num in count:
+            outdict[num] = news_singleton(num)
 
+    # Each thread will get 'chunksize' nums and its own output dict
+    chunksize = int(math.ceil(len(count) / float(nthreads)))
+    threads = []
+    outs = [{} for i in range(nthreads)]
+
+    for i in range(nthreads):
+        # Create each thread, passing it its chunk of numbers to factor
+        # and output dict.
+        t = threading.Thread(
+                target=worker,
+                args=(count[chunksize * i:chunksize * (i + 1)],
+                      outs[i]))
+        threads.append(t)
+        t.start()
+
+    # Wait for all threads to finish
+    for t in threads:
+        t.join()
+
+    # Merge all partial output dicts into a single dict and return it
+    output = ({k: v for out_d in outs for k, v in out_d.items()}).values()
+
+    return render_template('news.html', news=output, blocked=blocked_terms)
+
+@application.route("/portfolio")
+def portfolio():
+    return render_template('portfolio.html')
+
+@application.route("/research")
+def research():
+    return render_template('research.html')
+
+@application.route("/resume")
+def resume():
+    return render_template('resume.html')
 
 # run the app.
 if __name__ == "__main__":
