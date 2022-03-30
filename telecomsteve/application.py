8c97474148511f171ffb82dff02d633f56cf6ecb
Steven Platt <31355889+stevenplatt@users.noreply.github.com>
2022-03-30 16:19:06 -0700

    Update application.py
    updated port to specify 8080

--- telecomsteve/application.py
+++ telecomsteve/application.py
@@ -143,4 +143,4 @@ if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     application.debug = False
-    application.run  (host= '0.0.0.0') # (host="localhost", port=8000) # # #  #  # 
+    application.run  (host= '0.0.0.0', port=8080) # (host="localhost", port=8000) # # #  #  # 
