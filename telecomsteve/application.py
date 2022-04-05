d8d11f5ce9d9728db7b9d538fdfdb8dc2497b1fb
Steven Platt <mr.platt@gmail.com>
2022-04-04 17:45:43 -0700

    updated application port

--- telecomsteve/application.py
+++ telecomsteve/application.py
@@ -83,4 +83,4 @@ if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     application.debug = False
-    application.run (host="localhost", port=8000) #(host= '0.0.0.0', port=8080) #  #  # #  #  # 
+    application.run (host= '0.0.0.0', port=8080) # (host="localhost", port=8000) 
