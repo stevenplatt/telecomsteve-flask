6f9653938fd6d0294f8156d34a223800c71cdc8a
Steven Platt <mr.platt@gmail.com>
2022-04-04 17:30:54 -0700

    removal of hackernews.py application

--- telecomsteve/application.py
+++ telecomsteve/application.py
@@ -2,11 +2,10 @@
 # https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/create-deploy-python-flask.html
 # push updated deployment using 'eb deploy' from within the folder telecomsteve/
 
-import os, threading, math, requests, feedparser, random
+import os, feedparser, random
 import arxivpy
 from flask import Flask, render_template, request, url_for
 from urllib.parse import urlparse
-from hackernews import HackerNews
 
 
 application = Flask(__name__)
@@ -78,73 +77,6 @@ def login():
 
     else:
         return render_template('login.html', message=welcome_message)
-
-@application.route("/news", methods=["GET"])
-def news():
-    # list of domains and titles to filter from the news feed
-    blocked_terms = ['twitter.com', 'bloomberg.com', 'nytimes.com', 'wsj.com', 
-                    'ft.com', 'trump', 'hiring', 'economist.com', 'reuters.com']
-    count = range(50) # number of stories to display
-    nthreads = 50 # number 
-
-    def news_singleton(num):
-        hn = HackerNews()
-        stories = hn.top_stories()
-        news_dict = {"Title":[],"URL":[],"Domain":[], "Score":[]}
-
-        try:
-            website = hn.item(stories[num])
-
-            # instructions: https://stackoverflow.com/questions/1521592/get-root-domain-of-link
-            parsed_uri = urlparse(str(website.url))
-            domain = '{uri.netloc}'.format(uri=parsed_uri)
-            domain = domain.replace('www.', '')
-            
-            if len(domain) > 23: 
-                domain = domain[:20] + '...' # get the first 20 charecters of the URL and append '...'
-            else:
-                pass
-
-            # add story details to dictionary
-            news_dict["Title"] = (str(website.title))
-            news_dict["URL"] = (str(website.url))
-            news_dict["Domain"] = domain
-            news_dict["Score"] = (str(website.score))
-        except:
-            pass
-        
-        return(news_dict)
-
-    def worker(count, outdict):
-        """ The worker function, invoked in a thread. 'nums' is a
-            list of numbers to factor. The results are placed in
-            outdict.
-        """
-        for num in count:
-            outdict[num] = news_singleton(num)
-
-    # Each thread will get 'chunksize' nums and its own output dict
-    chunksize = int(math.ceil(len(count) / float(nthreads)))
-    threads = []
-    outs = [{} for i in range(nthreads)]
-
-    for i in range(nthreads):
-        # Create each thread, passing it its chunk of numbers to factor
-        # and output dict.
-        t = threading.Thread(
-                target=worker,
-                args=(count[chunksize * i:chunksize * (i + 1)], outs[i]))
-        threads.append(t)
-        t.start()
-
-    # Wait for all threads to finish
-    for t in threads:
-        t.join()
-
-    # Merge all partial output dicts into a single dict and return it
-    output = ({k: v for out_d in outs for k, v in out_d.items()}).values()
-
-    return render_template('news.html', news=output, blocked=blocked_terms)
 
 # run the app.
 if __name__ == "__main__":
