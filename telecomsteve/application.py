0ee0e52b954d4c080894c391f966f9028b1f188d
Steven Platt <31355889+stevenplatt@users.noreply.github.com>
2024-02-26 10:10:55 -0500

    added "feeds" route

--- telecomsteve/application.py
+++ telecomsteve/application.py
@@ -26,6 +26,7 @@ def research():
     return render_template('research.html')
 
 @application.route("/news", methods=["GET"])
+@application.route("/feeds", methods=["GET"])
 @application.route("/engineering", methods=["GET"])
 def engineering():
     content = newsfeed('engineering')
