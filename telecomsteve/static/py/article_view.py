2be52295034bc71b7ae21ebb8dd161b19116a7be
Steven Platt <31355889+stevenplatt@users.noreply.github.com>
2024-02-28 15:04:01 -0500

    .

--- telecomsteve/static/py/article_view.py
+++ telecomsteve/static/py/article_view.py
@@ -8,7 +8,7 @@ def article(url):
     byline = article['byline']
 
     content = article['content']
-    content = '<h1>' + content.split('<h1>', 1)[-1] # Split the content at the first occurrence of '<h1>' and take everything after it
+    content = content.split('</h1>', 1)[-1] # Split the content at the first occurrence of '</h1>' and take everything after it
 
     plain_text = article['plain_text']
     plain_content = article['plain_content']
