a42b9001b8bcb41732ce9e908d80e5b4cfa14997
Steven Platt <31355889+stevenplatt@users.noreply.github.com>
2024-02-28 13:45:40 -0500

    experimental split of feed html content

--- telecomsteve/static/py/article_view.py
+++ telecomsteve/static/py/article_view.py
@@ -6,7 +6,10 @@ def article(url):
     article = simple_json_from_html_string(req.text, use_readability=True)
     title = article['title']
     byline = article['byline']
+
     content = article['content']
+    content = '<h1>' + content.split('<h1>', 1)[-1] # Split the content at the first occurrence of '<h1>' and take everything after it
+
     plain_text = article['plain_text']
     plain_content = article['plain_content']
     return title, byline, content, plain_text, plain_content
