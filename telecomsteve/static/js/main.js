cf7e41a516e3db4e437fb9844ac134ab17e8d3c4
Steven Platt <31355889+stevenplatt@users.noreply.github.com>
2024-03-30 19:31:41 -0400

    updating button CSS

--- telecomsteve/static/js/main.js
+++ telecomsteve/static/js/main.js
@@ -1,7 +1,7 @@
 // scroll to top function
 
 //Get the button
-var mybutton = document.getElementById("myBtn");
+var mybutton = document.getElementById("scrollBtn");
         
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
